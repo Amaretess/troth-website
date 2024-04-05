b9732402bef71527f5ab5cacbcfd1b05e8c553c7
Ashlyn Amaretess <108196943+Amaretess@users.noreply.github.com>
2024-04-04 20:27:03 -0700

    Updated 4/4

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -3,7 +3,8 @@
 const ErrorPage = () => {
     return (
         <div>
-
+            <h1>404 NOT FOUND</h1>
+            <p>Sorry the page you were looking for could not be found...</p>
         </div>
     )
 }
